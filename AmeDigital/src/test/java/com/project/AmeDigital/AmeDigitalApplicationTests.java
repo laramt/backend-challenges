95dcfe6fa994186a01aea2dfa370b1ad3cbb625a
Lara Menck <lara.menckt@gmail.com>
2023-05-15 12:09:15 -0300

    first commit

---
+++ AmeDigital/src/test/java/com/project/AmeDigital/AmeDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.AmeDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmeDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
