f415a2a01481b899aecd3743311e843bca2d14bd
Lara Menck <lara.menckt@gmail.com>
2023-06-28 10:01:21 -0300

    repository refactor

--- AmeDigital/src/main/java/com/project/AmeDigital/repositories/PlanetRepository.java
+++ AmeDigital/src/main/java/com/project/AmeDigital/repositories/PlanetRepository.java
@@ -1,11 +1,8 @@
 package com.project.AmeDigital.repositories;
 
 import com.project.AmeDigital.models.Planet;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PlanetRepository extends JpaRepository<Planet, Long> {
-    Optional findByName(String name);
+    Planet findByName(String name);
 }
